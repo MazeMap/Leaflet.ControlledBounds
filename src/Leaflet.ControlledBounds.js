d539b272005abacfaef64ddd47d0f56d265d4fea
Fabien Girard <fabien@mapotempo.com>
2015-08-20 17:03:18 +0200

    Take into account global offsets of corners #3

--- src/Leaflet.ControlledBounds.js
+++ src/Leaflet.ControlledBounds.js
@@ -22,17 +22,17 @@
 			// Oh $deity, what a mess of spaghetti code and casuistics.
 			var size = this.getSize();
 			var candidates = {
-				topleft:     [L.point(0, 0)],
-				topright:    [L.point(size.x, 0)],
-				bottomleft:  [L.point(0, size.y)],
-				bottomright: [L.point(size.x, size.y)]
+				topleft:     [L.point(this._controlCorners.topleft.offsetLeft, this._controlCorners.topleft.offsetTop)],
+				topright:    [L.point(this._controlCorners.topright.offsetLeft + this._controlCorners.topright.offsetWidth, this._controlCorners.topright.offsetTop)],
+				bottomleft:  [L.point(this._controlCorners.bottomleft.offsetLeft, this._controlCorners.bottomleft.offsetTop + this._controlCorners.bottomleft.offsetHeight)],
+				bottomright: [L.point(this._controlCorners.bottomright.offsetLeft + this._controlCorners.bottomright.offsetWidth, this._controlCorners.bottomright.offsetTop + this._controlCorners.bottomright.offsetHeight)]
 			};
-			var previousBottom = 0;
+			var previousBottom = this._controlCorners.topleft.offsetTop;
 			for (var i=0; i<this._controlCorners.topleft.children.length; i++) {
 				var child = this._controlCorners.topleft.children[i];
 				if (child.offsetTop + child.offsetLeft + child.offsetWidth + child.offsetHeight > 0) {
 					var childTop    = child.offsetTop;
-					var childLeft   = child.offsetLeft;
+					var childLeft   = child.offsetLeft + this._controlCorners.topleft.offsetLeft;
 					var childBottom = childTop + child.offsetHeight;
 					var childRight  = childLeft + child.offsetWidth;
 
@@ -43,13 +43,13 @@
 						}
 					}
 					candidates.topleft.push(L.point(childRight, previousBottom));
-					candidates.topleft.push(L.point(0, childBottom));
+					candidates.topleft.push(L.point(this._controlCorners.topleft.offsetLeft, childBottom));
 
 					previousBottom = childBottom;
 				}
 			}
 
-			previousBottom = 0;
+			previousBottom = this._controlCorners.topright.offsetTop;
 			for (var i=0; i<this._controlCorners.topright.children.length; i++) {
 				var child = this._controlCorners.topright.children[i];
 				if (child.offsetTop + child.offsetLeft + child.offsetWidth + child.offsetHeight > 0) {
@@ -65,18 +65,18 @@
 						}
 					}
 					candidates.topright.push(L.point(childLeft, previousBottom));
-					candidates.topright.push(L.point(size.x, childBottom));
+					candidates.topright.push(L.point(this._controlCorners.topright.offsetLeft + this._controlCorners.topright.offsetWidth, childBottom));
 
 					previousBottom = childBottom;
 				}
 			}
 
-			var previousTop = size.y;
+			var previousTop = this._controlCorners.bottomleft.offsetTop;
 			for (var i=this._controlCorners.bottomleft.children.length - 1; i>=0; i--) {
 				var child = this._controlCorners.bottomleft.children[i];
 				if (child.offsetTop + child.offsetLeft + child.offsetWidth + child.offsetHeight > 0) {
 					var childTop    = child.offsetTop + this._controlCorners.bottomleft.offsetTop;
-					var childLeft   = child.offsetLeft;
+					var childLeft   = child.offsetLeft + this._controlCorners.bottomleft.offsetLeft;
 					var childBottom = childTop + child.offsetHeight;
 					var childRight  = childLeft + child.offsetWidth;
 
@@ -87,13 +87,13 @@
 						}
 					}
 					candidates.bottomleft.push(L.point(childRight, previousTop));
-					candidates.bottomleft.push(L.point(0, childTop));
+					candidates.bottomleft.push(L.point(this._controlCorners.bottomleft.offsetLeft, childTop));
 
 					previousTop = childTop;
 				}
 			}
 
-			previousTop = size.y;
+			previousTop = this._controlCorners.bottomright.offsetTop;
 			for (var i=this._controlCorners.bottomright.children.length - 1; i>=0; i--) {
 				var child = this._controlCorners.bottomright.children[i];
 				if (child.offsetTop + child.offsetLeft + child.offsetWidth + child.offsetHeight > 0) {
@@ -109,7 +109,7 @@
 						}
 					}
 					candidates.bottomright.push(L.point(childLeft, previousTop));
-					candidates.bottomright.push(L.point(size.x, childTop));
+					candidates.bottomright.push(L.point(this._controlCorners.bottomright.offsetLeft + this._controlCorners.bottomright.offsetWidth, childTop));
 
 					previousTop = childTop;
 				}
@@ -121,7 +121,6 @@
 			candidates.bottomright = candidates.bottomright.filter(function(i){return !!i;});
 
 // 			console.log(candidates);
-
 
 			// Try out all combinations of the candidate corners, stick with the one that gives
 			//   out the biggest area.

2f27ab59858e0827edffc33d0cce43e4d531e2a4
Iván Sánchez Ortega <ivan@mazemap.no>
2015-08-20 13:43:59 +0200

    Fixed compatibility for Leaflet 1.0-beta1

--- src/Leaflet.ControlledBounds.js
+++ src/Leaflet.ControlledBounds.js
@@ -14,8 +14,6 @@
 			return this._controlledBounds;
 		},
 
-
-
 		_calculateControlledBounds: function() {
 			// Given the positions of the controls relative to the control container
 			//   (which is the controls' offsetParent), calculate corners which are good
@@ -122,7 +120,7 @@
 			candidates.bottomleft  = candidates.bottomleft.filter(function(i){return !!i;});
 			candidates.bottomright = candidates.bottomright.filter(function(i){return !!i;});
 
-	// 		console.log(candidates);
+// 			console.log(candidates);
 
 
 			// Try out all combinations of the candidate corners, stick with the one that gives
@@ -174,81 +172,76 @@
 			return this._controlledBounds = candidateBounds;
 		},
 
-		getBounds: function() {
-			if (!this._controlledBounds) {
-				this._calculateControlledBounds();
-			}
-			var origin = this.getPixelOrigin();
-
-			var min = this.unproject([origin.x + this._controlledBounds.min.x,
-									origin.y + this._controlledBounds.min.y]);
-			var max = this.unproject([origin.x + this._controlledBounds.max.x,
-									origin.y + this._controlledBounds.max.y]);
-
-			return new L.LatLngBounds(min, max);
-		},
-
-
 		getOffset: function() {
 			return this._controlledOffset ? this._controlledOffset : L.point(0, 0);
 		},
 
-		getCenter: function () {
-			if (!this._controlledBounds) {
-				return mapProto.getCenter.call(this);
-			}
 
-			return this.containerPointToLatLng(this._controlledBounds.getCenter());
+		_offsetLatLng: function (latlng, zoom) {
+			zoom = zoom === undefined ? this.getZoom() : zoom;
+			return this.unproject(
+				this.project(latlng, zoom).add(this.getOffset()),
+				zoom
+			);
 		},
 
-		setView: function (center, zoom, options) {
-			center = L.latLng(center);
-	// 		this._calculateControlledBounds();
-
-			if (this._controlledBounds) {
-				var point = this.project(center, this._limitZoom(zoom));
-				point = point.add(this.getOffset());
-				center = this.unproject(point, this._limitZoom(zoom));
-			}
-
-			return mapProto.setView.call(this, center, zoom, options);
+		_deoffsetLatLng: function (latlng, zoom) {
+			zoom = zoom === undefined ? this.getZoom() : zoom;
+			return this.unproject(
+				this.project(latlng, zoom).subtract(this.getOffset()),
+				zoom
+			);
 		},
 
+		setView: function (center, zoom) {
+			return mapProto.setView.call(this, this._offsetLatLng(center, zoom), zoom);
+		},
+
+
+		// setZoomAround needs to *undo* the changes introduced to `setView`
 		setZoomAround: function (latlng, zoom, options) {
-			if (this._controlledBounds) {
-				var scale = this.getZoomScale(zoom),
-					viewHalf = this._controlledBounds.getCenter(),
-					containerPoint = latlng instanceof L.Point ? latlng : this.latLngToContainerPoint(latlng),
+			var scale = this.getZoomScale(zoom),
+				viewHalf = this.getSize().divideBy(2).subtract(this.getOffset()),
+				containerPoint = latlng instanceof L.Point ? latlng : this.latLngToContainerPoint(latlng),
 
-					centerOffset = containerPoint.subtract(viewHalf).multiplyBy(1 - 1 / scale),
-					newCenter = this.containerPointToLatLng(viewHalf.add(centerOffset));
+				centerOffset = containerPoint.subtract(viewHalf).multiplyBy(1 - 1 / scale),
+				newCenter = this.containerPointToLatLng(viewHalf.add(centerOffset));
 
-				return this.setView(newCenter, zoom, {zoom: options});
-			} else {
-				return mapProto.setZoomAround.call(this, latlng, zoom, options);
-			}
+			return this.setView(newCenter, zoom, {zoom: options});
 		},
 
 
-		getBoundsZoom: function (bounds, inside, padding) { // (LatLngBounds[, Boolean, Point]) -> Number
+		// setZoom needs to *undo* the changes introduced to `setView`
+		setZoom: function (zoom, options) {
+			if (!this._loaded) {
+				this._zoom = zoom;
+				return this;
+			}
+			return this.setView(this._deoffsetLatLng(this.getCenter()), zoom, {zoom: options});
+		},
+
+
+		// A shameless copy of Map.getBoundsZoom with a reference to _controlledBounds
+		// which sets the size.
+		getBoundsZoom: function (bounds, inside, padding) {
 			bounds = L.latLngBounds(bounds);
 
 			var zoom = this.getMinZoom() - (inside ? 1 : 0),
-				maxZoom = this.getMaxZoom(),
-				cb = this._controlledBounds,
-				size = (cb) ? L.point(cb.max.x - cb.min.x, cb.max.y - cb.min.y) : this.getSize(),
+			    maxZoom = this.getMaxZoom(),
+			    cb = this._controlledBounds,
+			    size = (cb) ? L.point(cb.max.x - cb.min.x, cb.max.y - cb.min.y) : this.getSize(),
 
-				nw = bounds.getNorthWest(),
-				se = bounds.getSouthEast(),
+			    nw = bounds.getNorthWest(),
+			    se = bounds.getSouthEast(),
 
-				zoomNotFound = true,
-				boundsSize;
+			    zoomNotFound = true,
+			    boundsSize;
 
 			padding = L.point(padding || [0, 0]);
 
 			do {
 				zoom++;
-				boundsSize = this.project(se, zoom).subtract(this.project(nw, zoom)).add(padding);
+				boundsSize = this.project(se, zoom).subtract(this.project(nw, zoom)).add(padding).floor();
 				zoomNotFound = !inside ? size.contains(boundsSize) : boundsSize.x < size.x || boundsSize.y < size.y;
 
 			} while (zoomNotFound && zoom <= maxZoom);
@@ -260,71 +253,19 @@
 			return inside ? zoom : zoom - 1;
 		},
 
-		getBounds: function() {
-			if (this._controlledBounds) {
-				return L.latLngBounds(
-					this.containerPointToLatLng(this._controlledBounds.min),
-					this.containerPointToLatLng(this._controlledBounds.max)
-				);
-			} else {
-				return mapProto.getBounds.call(this);
-			}
-		},
 
-		invalidateSize: function (options) {
+		invalidateSize: function() {
 			if (!this._loaded) { return this; }
 
-			options = L.extend({
-				animate: false,
-				pan: true
-			}, options === true ? {animate: true} : options);
+			var oldOffset = this.getOffset();
 
-			var oldCenter, oldSize;
-			if (this._controlledBounds) {
-				oldCenter = this._controlledBounds ? this._controlledBounds.getCenter() : this.getSize().divideBy(2).round();
-				oldSize = L.point(this._controlledBounds.max.x -
-								this._controlledBounds.min.x,
-								this._controlledBounds.max.y -
-								this._controlledBounds.min.y);
-			} else {
-				oldSize = this.getSize();
-				oldCenter = oldSize.divideBy(2).round();
-			}
-			this._sizeChanged = true;
-			this._initialCenter = null;
+			mapProto.invalidateSize.call(this);
+
 			this._calculateControlledBounds();
 
-			var newCenter = this._lastControlledBounds.getCenter();
-			var newSize = L.point(this._controlledBounds.max.x - this._controlledBounds.min.x,
-								this._controlledBounds.max.y - this._controlledBounds.min.y);
-
-			var offset = oldCenter.subtract(newCenter);
-
-			if (!offset.x && !offset.y) { return this; }
-
-			if (options.animate && options.pan) {
-				this.panBy(offset);
-
-			} else {
-				if (options.pan) {
-					this._rawPanBy(offset);
-				}
-
-				this.fire('move');
-
-				if (options.debounceMoveend) {
-					clearTimeout(this._sizeTimer);
-					this._sizeTimer = setTimeout(L.bind(this.fire, this, 'moveend'), 200);
-				} else {
-					this.fire('moveend');
-				}
-			}
-
-			return this.fire('resize', {
-				oldSize: oldSize,
-				newSize: newSize
-			});
+			this.panBy(this.getOffset().subtract(oldOffset));
 		}
+
 
 	});
 
